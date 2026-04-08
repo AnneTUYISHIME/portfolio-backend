5760e5e9cf56ca7efd2407f528d5ad984683dbfd
Anne TUYISHIME <atuyishime28@gmail.com>
2026-04-09 01:17:21 +0200

    email logic done

--- src/main/java/com/anne/PortfolioBackendApplication.java
+++ src/main/java/com/anne/PortfolioBackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.portfolio_backend;
+package com.anne.portfolio_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class PortfolioBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioBackendApplication.class, args);
 	}
-
 }
